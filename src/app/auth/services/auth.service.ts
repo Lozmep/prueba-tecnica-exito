3a73a7f0ed6c5745a3839a15147607fbda6ab0b5
Lozmep <lozmep.adm@gmail.com>
2025-02-12 01:23:38 -0500

    Add method to generate fake jwt token

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,15 +1,64 @@
 import { Injectable } from '@angular/core'
+import { LoginResponse } from '@auth/interfaces/login-response.interface'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private tokenKey = 'authToken'
 
-  login(username: string, password: string): boolean {
+  login(username: string, password: string): LoginResponse {
     if (username === 'admin' && password === 'admin123') {
-      return true;
+      const fakeToken = this.generateFakeToken()
+      const result: LoginResponse = { ok: true, status: 200, token: fakeToken }
+      if (result.ok) {
+        this.setToken(fakeToken)
+        return result
+      }
+      return { ok: true, status: 200, token: fakeToken }
     }
-    return false;
+    return { ok: false, status: 401, token: '' }
+  }
+  private generateFakeToken(): string {
+    const header = btoa(JSON.stringify({ alg: 'HS256', typ: 'JWT' }))
+    const payload = btoa(JSON.stringify({ username: 'admin', role: 'admin', exp: Math.floor(Date.now() / 1000) + 3600 }))
+    const signature = 'fake-signature'
+    return `${header}.${payload}.${signature}`
+  }
+
+  private setToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token)
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey)
+  }
+
+  isAuthenticated(): boolean {
+    if (this.isTokenExpired()) {
+      this.logout()
+      return false
+    }
+    return true
+  }
+  isTokenExpired(): boolean {
+    const token = this.getToken()
+    if (!token) return true
+
+    try {
+      const payloadBase64 = token.split('.')[1]
+      const payloadJson = JSON.parse(atob(payloadBase64))
+      const exp = payloadJson.exp
+
+      return Date.now() / 1000 > exp
+    } catch (error) {
+      console.error('Error al decodificar el token:', error)
+      return true
+    }
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.tokenKey)
   }
 
 }
