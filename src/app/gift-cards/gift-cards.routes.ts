47a38ec9d0b86f13001b79e017ccf91136dfbc86
Lozmep <lozmep.adm@gmail.com>
2025-02-12 01:24:31 -0500

    Add guard to gift cards routes

--- src/app/gift-cards/gift-cards.routes.ts
+++ src/app/gift-cards/gift-cards.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router'
 import { ExamplePageComponent } from './pages/example-page/example-page.component'
+import { authGuard } from '@auth/guards/auth.guard'
 
 export const gitfCardsRoutes: Routes = [
   {
     path: '',
+    canActivate: [authGuard],
     children: [
       {
         path: 'pool',
